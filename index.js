fc14f59293906fb761bda107c1e2e248c4575dfb
NABIJON <79036335+Nabijon2001@users.noreply.github.com>
2022-05-14 20:17:15 +0500

    required categories and used category api

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const categories = require("./routers/categories");
 
+// ***** mongodb connection****************//
 mongoose
   .connect("mongodb://localhost:27017/food_drinks_market")
   .then(() => {
@@ -9,6 +11,8 @@ mongoose
   })
   .catch((error) => console.log(`Connection Error..., ${error}`));
 
+app.use(express.json());
+app.use("/api/categories", categories);
 const port = process.env.PORT | 3000;
 
 app.listen(port, () => {
