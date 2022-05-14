65939a33ea01476b7b8d4dec2943300fbf472bae
NABIJON <79036335+Nabijon2001@users.noreply.github.com>
2022-05-14 16:48:53 +0500

    created categories schema

--- models/categories.js
+++ models/categories.js
@@ -1,1 +1,21 @@
 const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const CategorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 50,
+  },
+});
+function ValidateCategory(category) {
+  const schema = {
+    name: Joi.string().min(3).max(50).required(),
+  };
+  return Joi.validate(category, schema);
+}
+const Category = mongoose.model("Category", CategorySchema);
+
+exports.Category = Category;
+exports.validate = ValidateCategory;
