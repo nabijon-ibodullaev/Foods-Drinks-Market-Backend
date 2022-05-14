640c6a0903dbfaa25a2fc1f09f5fa1bed58a76bc
NABIJON <79036335+Nabijon2001@users.noreply.github.com>
2022-05-14 16:06:40 +0500

    MongoDB is connected...

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose.connect(
+  "mongodb://localhost:27017",
+  () => {
+    console.log("mongodb is connected...");
+  },
+  (err) => {
+    console.log(`Connection error ${err}`);
+  }
+);
 
 app.get("/api/course", (req, res) => {
   res.send("Hello World");
