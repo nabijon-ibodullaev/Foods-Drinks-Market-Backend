b75a3d2a9cd5affa96dce665e9448b43365bc7fb
NABIJON <79036335+Nabijon2001@users.noreply.github.com>
2022-05-14 15:52:04 +0500

    added package-lock.json to .gitignore

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 
-app.get('/api/course', (req, res) => {
-    res.send('Hello World');
-})
+app.get("/api/course", (req, res) => {
+  res.send("Hello World");
+});
 
 const port = process.env.PORT | 3000;
 
 app.listen(port, () => {
-    console.log(`App listening - ${port}`)
-})
+  console.log(`App listening - ${port}`);
+});
