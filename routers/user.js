34d1d70c4cf9035c164fb276a3d76a97a103ac8f
NABIJON <79036335+Nabijon2001@users.noreply.github.com>
2022-05-15 16:53:07 +0500

    joined bcrypt to hash user's password

--- routers/user.js
+++ routers/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { User } = require("../models/user");
 const _ = require("lodash");
+const bcrypt = require("bcrypt");
 
 router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
@@ -9,6 +10,8 @@ router.post("/", async (req, res) => {
     return res.status(400).send("Exist User");
   }
   user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt();
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
   res.send(_.pick(user, ["_id", "name", "email"]));
 });
