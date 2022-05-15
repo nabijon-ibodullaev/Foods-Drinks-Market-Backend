5e8432e533d1ed3327c27f2448deac645ffad8f2
NABIJON <79036335+Nabijon2001@users.noreply.github.com>
2022-05-15 17:35:25 +0500

    joined json web token creator

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -21,7 +23,10 @@ const userSchema = new mongoose.Schema({
     max: 1024,
   },
 });
-
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, config.get("jwtPrivateKey"));
+  return token;
+};
 const User = mongoose.model("User", userSchema);
 
 exports.User = User;
