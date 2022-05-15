3bd764626a09fbcb106585781590b8bebb0f9ce8
NABIJON <79036335+Nabijon2001@users.noreply.github.com>
2022-05-15 08:11:26 +0500

    added post method

--- routers/food.js
+++ routers/food.js
@@ -10,4 +10,17 @@ router.get("/", async (req, res) => {
   res.send(foods);
 });
 
+router.post("/", async (req, res) => {
+  let food = new Food({
+    name: req.body.name,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    rating: req.body.rating,
+    categoryName: req.body.categoryName,
+  });
+  food = await food.save();
+
+  res.status(201).send(food);
+});
 module.exports = router;
