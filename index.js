b78117983a99447ec950eba237ab0edc9a4723d5
NABIJON <79036335+Nabijon2001@users.noreply.github.com>
2022-05-15 07:46:31 +0500

    added foods router

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const categories = require("./routers/categories");
+const foods = require("./routers/food");
 
-// ***** mongodb connection*********//
+// ********** mongodb connection ************//
 mongoose
   .connect("mongodb://localhost:27017/food_drinks_market")
   .then(() => {
@@ -11,10 +12,13 @@ mongoose
   })
   .catch((error) => console.log(`Connection Error..., ${error}`));
 
+// ********** MIDDLEWARE *********//
 app.use(express.json());
 app.use("/api/categories", categories);
-const port = process.env.PORT | 3000;
+app.use("/api/foods", foods);
 
+//************* PORT ********************* */
+const port = process.env.PORT | 3000;
 app.listen(port, () => {
   console.log(`App listening - ${port}`);
 });
